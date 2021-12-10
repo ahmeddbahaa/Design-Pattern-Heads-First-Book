15b463d9460c0c5db0f623d9069b6331b96cdd2b
ahmeddbahaa <ahmedbahaa0026@gmail.com>
2021-12-10 07:34:40 +0200

    Decorator Design Pattern Added

---
+++ DecoratorDesignPattern/src/HouseBlend.java
@@ -0,0 +1,14 @@
+
+public class HouseBlend extends Beverage{
+
+	public HouseBlend()
+	{
+		description = "HouseBlend";
+	}
+	@Override
+	public double cost() {
+		// TODO Auto-generated method stub
+		return 0.89;
+	}
+
+}
